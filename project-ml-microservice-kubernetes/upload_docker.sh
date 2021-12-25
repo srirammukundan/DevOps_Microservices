ec6556cac4b01fbc8d806f676e11dfbe97afd34e
srirammukundan <sriram.mukundan1984@gmail.com>
2021-12-25 14:32:31 +0000

    changes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+docker tag dockerss1/udacity:p4udacity dockerss1/udacity:p4udacity-image
+dockerpath=dockerss1/udacity:p4udacity-image
 
 # Step 2:  
 # Authenticate & tag
+docker login -u dockerss1
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push dockerss1/udacity:p4udacity-image
